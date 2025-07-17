db52152dfceb3fa39a89249366b4b28a3d23c8ad
maxfa5 <maksfa04@mail.ru>
2025-07-17 20:54:47 +0500

    add logger

--- src/main/java/org/project/service/CommandHandlerService.java
+++ src/main/java/org/project/service/CommandHandlerService.java
@@ -1,5 +1,24 @@
 package org.project.service;
 
-public class CommandHandlerService   {
-    
+import org.project.model.Command;
+import org.project.model.Priority;
+import org.project.queue.CommandQueue;
+
+public class CommandHandlerService {
+    private final CommandQueue commandQueue = new CommandQueue();
+
+    public void handleCommand(Command command) {
+        if (command.getPriority() == Priority.CRITICAL) {
+            executeCommand(command);
+        } else {
+            boolean added = commandQueue.addCommand(command);
+            if (!added) {
+                throw new RuntimeException("Command queue is full");
+            }
+        }
+    }
+
+    private void executeCommand(Command command) {
+        // Логика исполнения команды
+    }
 }
